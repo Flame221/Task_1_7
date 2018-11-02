8f279e1c205024162068f1b4b4a4547d8f4ba231
Flame221 <43928831+Flame221@users.noreply.github.com>
2018-11-02 20:37:26 +0200

    Update Main3.java

--- src/main/java/com/yourname/Main3.java
+++ src/main/java/com/yourname/Main3.java
@@ -7,34 +7,33 @@ public class Main3 {
         int z;
         for (int num = 1; num <= 50000; num++) {
             z = num;
+            while(z >= 10){
             if (z % 10 == 2) {
                 s++;
                 continue;
-            } else {
+             } else {
                 z /= 10;
+             }
             }
 
             if (z % 10 == 2) {
-                s++;
-                continue;
-            } else {
-                z /= 10;
-            }
+package com.yourname;
+
+//Программа 5
+public class Main3 {
+    public static void main(String[] args) {
+        int s = 0;
+        int z;
+        for (int num = 1; num <= 50000; num++) {
+            z = num;
+            while(z >= 10){
             if (z % 10 == 2) {
                 s++;
                 continue;
             } else {
                 z /= 10;
             }
-            if (z % 10 == 2) {
-                s++;
-                continue;
-            } else {
-                z /= 10;
-            }
-            if (z % 10 == 2) {
-                s++;
-            }
+         }   
         }
         System.out.println("Нужно заменить " + s + " таблиц");
     }
